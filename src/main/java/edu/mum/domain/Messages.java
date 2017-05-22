3ce3f3fe5595d7369e5f755f4b85fa736030f4f1
bero <bero@10.10.11.48>
2017-05-22 16:11:32 -0500

    Message Class Modification Alber@5/22/2017

--- src/main/java/edu/mum/domain/Messages.java
+++ src/main/java/edu/mum/domain/Messages.java
@@ -19,6 +19,26 @@ public class Messages {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
+	private User sender;
+	
+	private User receiver;
+	
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	public User getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+	
 	@Column(name = "MESSAGE", nullable = false)
 	private String message;
 
